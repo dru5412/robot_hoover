46559b5c2989adba3ea18865736c844a88efb171
dru5412 <dru5412@gmail.com>
2017-06-26 09:46:21 +0100

    Create robot_hoover.ino

---
+++ source/robot_hoover.ino
@@ -0,0 +1,33 @@
+//------------------------------------------------------------------------------
+//
+// Robot Hoover, Version 2.0
+// By Andrew Brown. 
+// GNU Open Licence 2017
+// Feel free to use, adapt and share code freely without restriction
+//
+//------------------------------------------------------------------------------
+
+/* TODO LIST
+
+1. Stepper motor control
+2. ultra sound distance measure
+3. stuck detection
+4. battery level check
+5. buzzer for alarms?
+6. Loop Routine template
+
+*/
+
+
+// GLOBAL DEFINITIONS
+
+#define motorsEnablePin 6
+#define motor1dirPin 2
+#define motor1pulsePin 3
+#define motor2dirPin 4
+#define motor2pulsePin 5
+#define fanEnablePin 7
+#define batteryMonitorPin A0
+
+
+
