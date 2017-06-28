5820940c8b605b9507afc8c7a40bdb5af531f807
dru5412 <dru5412@gmail.com>
2017-06-28 13:38:15 +0100

    Update robot_hoover.ino

--- source/robot_hoover.ino
+++ source/robot_hoover.ino
@@ -1,213 +1,111 @@
-//------------------------------------------------------------------------------
-//
-// Robot Hoover, Version 2.0
-// By Andrew Brown. 
-// GNU Open Licence 2017
-// Feel free to use, adapt and share code freely without restriction
-//
-//------------------------------------------------------------------------------
 
-/* TODO LIST
-
-1. stuck detection, after 4 attempts to turn and still below minimum distance. or just 10 loops without a distance.
-2. bumper detection
-3. buzzer for alarms?
-
-
-*/
-
-
-
+  // Vacuum Robot Ver2.0
+  // Andrew Brown
+  // Open Source, feel free to use, modify, share as you see fit.
+  // Last updated 28th Jun 2017
+  
+  
   #include <NewPing.h>
-  
-  // GLOBAL DEFINITIONS
-  
-  #define motorsEnablePin 6
-  #define motor1DirPin 2
-  #define motor1PulsePin 3
-  #define motor2DirPin 4
-  #define motor2PulsePin 5
-  #define fanEnablePin 7
-  #define batteryPin A0
-  #define ledPin 13
-  #define ledBattPin 1
-  
-  #define leftSonar 9
-  #define rightSonar 10
-
-  NewPing sonarLeft(leftSonar, leftSonar, 100);
-  NewPing sonarRight(rightSonar, rightSonar, 100);
-  
-  #define bumper1 11
-  #define bumper2 12
-  
 
 
-  //-------------------------- USER PARAMETERS --------------------------
 
-  const float voltageBatCharged = 12.88;      // Voltage measured when battery fully charged 
+ //-------------------------- USER PARAMETERS --------------------------
 
-  const int minDistance = 10;                    //Mínimun distance of the sensor
+  const float voltageBatCharged = 12.88;            // Voltage measured when battery fully charged 
 
-  const float battCutOffVolts = 9.8;          // Voltage at which to switch off the fan and motors to protect battery
+  const float battCutOffVolts = 10.5;               // Voltage at which to switch off the fan and motors to protect battery
+   
+  const int minDistance = 6;                        //Mínimun distance of the sensor before turning Default: 6
 
-  const int motorSpeed = 400;                 // Controls Hoover speed, Lower value = faster, between 200-1000 depending on your set up should work
-  
-  
+  int pwmSpeed = 170;                               //PWM output for motor speed, betwen 0(stop) and 255(full speed). Default : 200
+   
+  int motorDriftOffSet = 5;                         // Helps if constant turning while moving forwards. Due to motor differences. Start about 5% of PWM value
   //-------------------------- END OF USER PARAMETERS --------------------------
 
-  #define DEBUG 0
   
-  unsigned int runMode = 0;                     //run Mode 0=standby, 1=running, 2=stuck, 3=low batt, 4=end program
-
-  float distanceLeft, distanceRight;
-
-  // Variables will change:
-  int bumperLeft = 0;                         // variable for reading the pushbutton status
-  int bumperRight = 0;                        // variable for reading the pushbutton status
-
+  boolean control = true;
+  boolean stuckMode = false;
+  int counter, counter2, bumperState, encoderCheck, switchTurn, stuckTurn, lastTurn;
+ 
+  #define encoderPin 2
+  #define motor1Pin1  3
+  #define motor1Pin2  5
+  #define motor2Pin1  6
+  #define motor2Pin2  9
   
+  #define leftSonar 7
+  #define rightSonar 8
   
+  #define fanEnablePin 10
+  #define bumperPin 11
+  #define ledBattPin 12
+  #define ledPin 13
+ 
+  #define batteryPin A1
+  
+  NewPing sonarLeft(leftSonar, leftSonar, 200);
+  
+  NewPing sonarRight(rightSonar, rightSonar, 200);
 
+//////////////CODE/////////////
 void setup() {
-  
-  // Fan Motor
+  //Initialize outputs and inputs
+
+  //Fan motor as output
   pinMode(fanEnablePin, OUTPUT);
-
-
-  //Enable Motors pin
-  pinMode(motorsEnablePin, OUTPUT);
-  digitalWrite(motorsEnablePin, HIGH);      //Disable Motors at start up
-  
   //Motor1
-  pinMode(motor1DirPin, OUTPUT);
-  pinMode(motor1PulsePin, OUTPUT);
+  pinMode(motor1Pin1, OUTPUT);
+  pinMode(motor1Pin2, OUTPUT);
   //Motor2
-  pinMode(motor2DirPin, OUTPUT);
-  pinMode(motor2PulsePin, OUTPUT);
-
+  pinMode(motor2Pin1, OUTPUT);
+  pinMode(motor2Pin2, OUTPUT);
   //LED
-  pinMode(ledPin, OUTPUT);
+  pinMode(ledPin, OUTPUT);  
+  digitalWrite(ledPin, HIGH);
   pinMode(ledBattPin, OUTPUT);
-  digitalWrite(ledBattPin, HIGH); 
-
+  digitalWrite(ledBattPin, HIGH);
   
   //INPUTS
-  
-  
+  // initialize the pushbutton inputs 
   //Bumper
-  pinMode(bumper1, INPUT_PULLUP); 
-  pinMode(bumper2, INPUT_PULLUP); 
-  
- 
+  pinMode(bumperPin, INPUT_PULLUP); 
+  pinMode(encoderPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(encoderPin), encoderReset, CHANGE);
+
   //Batt
   pinMode(batteryPin, INPUT);
-
-
-  // START UP Process
-  
   // Initialize serial
-  Serial.begin(115200); 
+  Serial.begin(115200);    
+  
+  //Set PWM with offset 
+  pwmSpeed = pwmSpeed - abs(motorDriftOffSet);
 
+  
+  
+  ///////////////////////////////Wait////////////////////////////////////////
   //Wait about 5 s and initialize fan if voltage ok
   waitBlinking(5,1); //5 seconds at 1 Hz
+  //Crank (initialize the fan because the voltage drops when cranking)
+  if(readBattery(batteryPin)>11.0){
+    digitalWrite(fanEnablePin, HIGH); //Turn the Fan ON
+    delay(1000); //For 1000ms
+    digitalWrite(ledBattPin, LOW);
+  }
+  else {
+     control = false;
+     digitalWrite(ledBattPin, HIGH);
+    }
+} 
+//////////Functions To Use //////////
 
-  batteryCheck();
 
-    if (runMode == 1) {digitalWrite(fanEnablePin, HIGH);}     //Switch on Fan
-
-
-  unsigned long loop_timer = micros() + 4000;                                             //Set the loop_timer variable at the next end loop time
+// Called via interupt on the encoderPin. each time the wheel moves.
+void encoderReset(){
+  encoderCheck =0; 
 }
 
 
-// -------- END SET UP -------
-
-void loop() {
-    
-  if (runMode == 2) {     // Stuck Mode
-
-    //Switch off Fan, Flash Both LED's. Disable Motors
-    digitalWrite(fanEnablePin, LOW);
-    digitalWrite(motorsEnablePin, HIGH);
-    digitalWrite(ledPin, HIGH);
-    digitalWrite(ledBattPin, LOW);
-    delay(300);
-    digitalWrite(ledPin, LOW);
-    digitalWrite(ledBattPin, HIGH);
-    delay(300);
-  }
-  
-
- 
-  if (runMode == 4) {     // End of Time Limit, slow flash LED for 1 minute
-
-    digitalWrite(fanEnablePin, LOW);
-    digitalWrite(motorsEnablePin, HIGH);
-    waitBlinking(60,0.5); //5 seconds at 1 Hz
-     
-  }
-  
-  if (runMode == 1) {         //MAIN running LOOP
-    
-    //  Do Distance Measure
-    //  Check Bumpers
-    //  Do Forwards or Turn
-    //  If stuck turn around
-    //  If still stuck, go stuck mode.
-
-
-    //Distance Check
-    distanceLeft = sonarLeft.ping_cm();
-    distanceRight = sonarRight.ping_cm();    
-
-   
-    if (distanceLeft < minDistance && distanceRight < minDistance){     //Approaching head on.
-
-      if (distanceLeft < distanceRight){        //Turn away from cloest side
-        moveMotors(1,-1);
-      } else{
-        moveMotors(-1,1);
-      }
-      
-    }
-
-    else if (distanceLeft < minDistance) {       //Too close to something on left, move right a bit)
-
-      while (distanceLeft < minDistance){
-        moveMotors(0,-0.5);
-        distanceLeft = sonarLeft.ping_cm();
-      }
-      
-    }
-
-    else if (distanceRight < minDistance) {       //Too close to something on right, move left a bit)
-
-      while (distanceRight < minDistance){
-        moveMotors(-0.5,0);
-        distanceRight = sonarRight.ping_cm();
-      }
-      
-    } else {
-
-      // Move forwards 2 rotations
-      moveMotors(2,2);
-    }
-    
-    
-  }
-  
-  
- if (slow_loop_counter >= 20){
-
-    batteryCheck();
-
- }
-
-// -------- END LOOP -------
-
-
-void waitBlinking(int n, int frequency){
+void waitBlinking(int n, int frequency){ // Used to flash the main led
   //blink for n seconds at frequency hz
   for (int i=1; i <= n; i++){
     for(int j=1; j<=frequency; j++){
@@ -219,84 +117,218 @@ void waitBlinking(int n, int frequency){
    } 
 }
 
+//Manipulate direction according the desired movement of the motors
 
+void forwardMotors(int moveTime){  
+   analogWrite(motor1Pin1, pwmSpeed - motorDriftOffSet); 
+   analogWrite(motor1Pin2, 0); 
+   analogWrite(motor2Pin1, pwmSpeed + motorDriftOffSet); 
+   analogWrite(motor2Pin2, 0); 
+   delay(moveTime);
+}
 
-void  batteryCheck(){
-  int readInput, oneRead;
+void backRightMotors(int moveTime){ 
+   analogWrite(motor1Pin1, 0); 
+   analogWrite(motor1Pin2, pwmSpeed-50); 
+   analogWrite(motor2Pin1, 0);
+   analogWrite(motor2Pin2, 0); 
+   delay(moveTime);
+}
+void backLeftMotors(int moveTime){ 
+   analogWrite(motor1Pin1, 0); 
+   analogWrite(motor1Pin2, 0); 
+   analogWrite(motor2Pin1, 0);
+   analogWrite(motor2Pin2, pwmSpeed-50); 
+   delay(moveTime);
+}
+void rightMotors(int moveTime){ 
+   analogWrite(motor1Pin1, 0); 
+   analogWrite(motor1Pin2, pwmSpeed-50); 
+   analogWrite(motor2Pin1, pwmSpeed-50);
+   analogWrite(motor2Pin2, 0); 
+   delay(moveTime);
+}
+void leftMotors(int moveTime){ 
+   analogWrite(motor1Pin1, pwmSpeed-50); 
+   analogWrite(motor1Pin2, 0); 
+   analogWrite(motor2Pin1, 0);
+   analogWrite(motor2Pin2, pwmSpeed-50); 
+   delay(moveTime);
+}
+void backwardMotors(int moveTime){
+   analogWrite(motor1Pin1, 0); 
+   analogWrite(motor1Pin2, pwmSpeed+ motorDriftOffSet);
+   analogWrite(motor2Pin1, 0); 
+   analogWrite(motor2Pin2, pwmSpeed+ motorDriftOffSet); 
+   delay(moveTime);
+}
+void stopMotors(){ 
+   analogWrite(motor1Pin1, 0);
+   analogWrite(motor1Pin2, 0); 
+   analogWrite(motor2Pin1, 0); 
+   analogWrite(motor2Pin2, 0); 
+}
+
+float  readBattery(int input){
+  int readInput;
   float voltage;
-  
-  // Take average of 5 readings 
-  for (int i=0; i<5; i++){
-    oneRead = analogRead(batteryPin);
-    readInput = readInput + oneRead;
-  }
-  readInput = readInput / 5;  //Take the Average of the 5 readinds
-  
+  readInput = analogRead(input);
   voltage = (((readInput*4.9)/1000)*voltageBatCharged ) / 5; // resolution of analog input = 4.9mV per Voltage 
-  
-  if (DEBUG==1) {Serial.print(" Battery= ");  Serial.print(voltage);}
-  
-  if(voltage<=battCutOffVolts){ //battery limit of discharge, 
-    runMode = 3;
-    digitalWrite(ledBattPin, HIGH);       // Turn On Low Batt LED
-    digitalWrite(ledPin, LOW);            // Turn off Run LED
-    digitalWrite(motorsEnablePin, HIGH);       // Disable Motors
-    
-    }
-  else {
-    if(runMode==0){         //If Battery Ok and in standby mode, switich to ready mode
-        runMode=1;
-        digitalWrite(ledBattPin, LOW);       // Turn Off Low Batt LED
-        } 
-    }
-
-  
+  return voltage;
   } 
 
+void batteryControl(int input){
+  //Turn everything off in case the battery is low
+  float v_battery;
+  v_battery = readBattery(input);
+  if(v_battery<=battCutOffVolts){ 
+    control = false;
+    digitalWrite(ledBattPin, HIGH);
+    }
+  else {
+    //Do nothing Convention
+    digitalWrite(ledBattPin, LOW);
+    }
+}
+/////////////////////////////////////////////////MAIN CODE//////////////////////////////
+void loop(){
 
-  void moveMotors(float left,  float right){
 
-  // Calling this void with 2 variables, allows you to request 1 full rotation of the left or right or both wheels.
-  // The left, right variables equal the number of full rotations required .
-  // Negative numbers are reverse, positive are forwards direction.
+    //First read bumper state and distance measurements 
+    bumperState = digitalRead(bumperPin);
+    float distanceLeft = sonarLeft.ping_cm();
+    delay(1);
+    float distanceRight = sonarRight.ping_cm(); 
 
-    // For Full Step 1.8deg steppers, 200 steps equals 1 full rotation
-    // Change the 200 value if using different stepper motors or steps
+#if DEBUG>0
+    Serial.print("left:");
+    Serial.print(distanceLeft);
+    Serial.print(" right:");
+    Serial.println(distanceRight);
+    Serial.println(lastTurn);
+#endif
+  
+  // Main loop if control = TRUE.
+  if (control){
     
-    unsigned int rotationLeft = abs(left) * 200;
-    unsigned int rotationRight = abs(right) * 200;
+     digitalWrite(ledPin, HIGH); // Enable the main LED to show we are working.
 
-    // Set Direction
 
-    if (left<0) {digitalWrite(motor1DirPin, LOW);} else {digitalWrite(motor1DirPin, HIGH);}
-    if (right<0) {digitalWrite(motor2DirPin, LOW);} else {digitalWrite(motor2DirPin, HIGH);}
-    
+    if (encoderCheck >100) {
 
-    //Run motors, number of rotations based on passed variables.
-    //This runs if either motor requires a pulse
-    // In order to move both motors at same(almost) time, check rotation values are bigger than zero.
-    // Then delay low pulse by speed and decrease remaining pulses by 1.
-    // Once both rotation values are zero, movement is complete.
-    
+      backwardMotors(800);
+      if (distanceLeft < distanceRight){        //Turn away from closest side
+       rightMotors(1000);
+      } else{
+       leftMotors(1000);
+      }
 
-    if (rotationLeft>0 || rotationRight>0 ){
+      if (encoderCheck > 100 ){ //now its moved a bit check the encoder is now moving, if not then stuck!
+
+        control=false;
+        stuckMode = true;
+         Serial.println("!!!STUCK!!!");
+         exit;
+      }
       
-        if (rotationLeft > 0){
-          digitalWrite(motor1PulsePin,HIGH); 
-          rotationLeft --;
-        }
-    
-        if (rotationRight > 0){
-          digitalWrite(motor2PulsePin,HIGH); 
-          rotationRight --;
-        }
-    
-          delayMicroseconds(motorSpeed); 
-          digitalWrite(motor1PulsePin,LOW); 
-          digitalWrite(motor2PulsePin,LOW); 
-          delayMicroseconds(motorSpeed); 
-    }
-  
     }
 
+
+  if (counter>2){
+     backwardMotors(800);
+     if(lastTurn==2){ rightMotors(1100);Serial.println("back right");}
+     else {leftMotors(1100);Serial.println("back left");}
+     counter=0;
+     Serial.println("Counter 3");
+  }
+
+ if (distanceLeft < minDistance && distanceRight < minDistance){     //Approaching head on.
+
+      backwardMotors(800); //backward delay of 500ms
+      Serial.println("wall");
+      if (distanceLeft < distanceRight){        //Turn away from closest side
+       rightMotors(1000);
+      } else{
+       leftMotors(1000);
+      }
+      
+    }
+
+    else if (distanceLeft < minDistance) {       //Too close to something on left, move right a bit)
+
+        if (counter ==0){
+        backwardMotors(20);
+        backLeftMotors(400);
+        }
+        else if (counter==1){    
+          backwardMotors(20);
+          backLeftMotors(600);
+        }
+         counter++;
+         counter2=0;
+         Serial.println("sense left");
+         lastTurn=1;
+      
+      
+    }
+
+    else if (distanceRight < minDistance) {       //Too close to something on right, move left a bit)
+
+     
+        if (counter ==0){
+        backwardMotors(20);
+        backRightMotors(400);
+        }
+        else if (counter==1){    
+          backwardMotors(20);
+          backRightMotors(600);
+        }
+         counter++;
+         counter2=0;
+         Serial.println("sense right");
+         lastTurn=2;
+      
+      
+    }
+
+
+     if (bumperState==0){
+     
+      backwardMotors(500); //backward delay of 500ms
+      rightMotors(400);
+      Serial.print("  bump ");
+         
+    }
   
+      else {
+        forwardMotors(2);
+        counter2 ++;
+        encoderCheck ++;
+        if (counter2 >200){counter2=0; counter=0; Serial.println("counter reset");}
+           }
+  
+  
+      }
+  
+  else if (!control){
+    //If the battery is low, turn everything off
+    digitalWrite(fanEnablePin, LOW); //Turn the Fan OFF
+    stopMotors();
+
+    if (stuckMode == true){
+
+    digitalWrite(ledPin, HIGH);
+    digitalWrite(ledBattPin, LOW);
+    delay(200);
+    digitalWrite(ledPin, LOW);
+    digitalWrite(ledBattPin, HIGH);
+    delay(200);
+    
+    
+    } else {
+    waitBlinking(1,3);  //blink as warning 3hz in a loop
+    Serial.print(" Low Battery! ");
+    Serial.println();
+    }
+  }
+}
